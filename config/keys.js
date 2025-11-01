999f03457aac11e286766110e6dc4515bf6958a1
Ozobia Nwadibia <Ozobia.cn3@gmail.com>
2025-11-01 11:48:26 -0500

    Updated the config file

--- config/keys.js
+++ config/keys.js
@@ -1,9 +1,4 @@
-// use the format below for the connection string
-
-// mongodb+srv://<database name on Atlas>:<password>@cluster0.ekb8f.mongodb.net/<dbname (anything here)>?retryWrites=true&w=majority
-
-//const connectionString = `mongodb+srv://${process.env.DB_PROJECTNAME}:${process.env.DB_PASSWORD}@cluster0.ekb8f.mongodb.net/${process.env.DB_USERNAME}?retryWrites=true&w=majority`;
-
-const connectionString = `mongodb+srv://tiny-improvements-db:VLyHb5vCnZmsHQi@cluster0.uiwqres.mongodb.net/?appName=Cluster0`;
+// MongoDB Atlas connection string using environment variables
+const connectionString = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER}.mongodb.net/${process.env.MONGODB_DATABASE}?retryWrites=true&w=majority`;
 
 module.exports = connectionString;
