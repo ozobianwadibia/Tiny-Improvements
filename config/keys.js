ffa1039e4ce517c07cb2c8a7921113b7522751ca
Ozobia Nwadibia <Ozobia.cn3@gmail.com>
2025-11-01 11:28:49 -0500

    Updated outdated packages and mongodb atlas connection

--- config/keys.js
+++ config/keys.js
@@ -2,6 +2,8 @@
 
 // mongodb+srv://<database name on Atlas>:<password>@cluster0.ekb8f.mongodb.net/<dbname (anything here)>?retryWrites=true&w=majority
 
-const connectionString = `mongodb+srv://${process.env.DB_PROJECTNAME}:${process.env.DB_PASSWORD}@cluster0.ekb8f.mongodb.net/${process.env.DB_USERNAME}?retryWrites=true&w=majority`;
+//const connectionString = `mongodb+srv://${process.env.DB_PROJECTNAME}:${process.env.DB_PASSWORD}@cluster0.ekb8f.mongodb.net/${process.env.DB_USERNAME}?retryWrites=true&w=majority`;
+
+const connectionString = `mongodb+srv://tiny-improvements-db:VLyHb5vCnZmsHQi@cluster0.uiwqres.mongodb.net/?appName=Cluster0`;
 
 module.exports = connectionString;
